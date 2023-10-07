3b9ed9aab88aef10e95a365baec8552979161ccd
bymse <egorstrelov@gmail.com>
2023-10-07 18:30:53 +0300

    Init

---
+++ X509plorer.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace X509plorer.Site.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
